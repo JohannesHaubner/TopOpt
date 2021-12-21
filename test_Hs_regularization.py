7e2a231e95db2c20bf4c366f16ba0c8d6d6d7f1b
JohannesHaubner <haubnerj@simula.no>
2021-12-21 09:35:48 +0100

    Hs_test

---
+++ test_Hs_regularization.py
@@ -0,0 +1,21 @@
+import pytest
+import Hs_regularization
+import numpy as np
+
+from dolfin import *
+from dolfin_adjoint import *
+
+def test_Hs_matrix():
+    N = 15
+    delta = 1.0
+    sigma = 7./16
+    reg_Hs = Hs_regularization.AssembleHs(N, delta, sigma)
+    matrix = reg_Hs.get_matrix(1.0)
+
+    # check if matrix is symmetric
+    assert np.all(matrix.todense() - matrix.todense()== 0)
+    # check if positive definite
+    assert np.all(np.linalg.eigvals(matrix.todense())> 0)
+
+if __name__ == "__main__":
+    test_Hs_matrix()
